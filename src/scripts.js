79f6a94509e0fa36497d16f7af5a421f8c5ae9fb
Calli Herrmann <calliherrmann@gmail.com>
2023-06-07 15:22:21 -0400

    Add onload to set currentCustomer

--- src/scripts.js
+++ src/scripts.js
@@ -4,22 +4,39 @@ import './scss/styles.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import '../dist/images/Overlook-logo.png'
-
+import { getData } from './apiCalls';
 
 
 // I should see a dashboard page that shows me:
 // Any room bookings I have made (past or upcoming)
 // The total amount I have spent on rooms
-
+let currentCustomer;
 // step 1: get customer
     // usually this would be retrieved from the user logging in. 
     // for now, we will use http://localhost:3001/api/v1/customers/<id> where<id> will be a number of a customer’s id
+
+window.onload = () => {
+    getData('customers/1')
+    .then(response => {
+        currentCustomer = response
+        console.log(currentCustomer)
+    })
+}
+
+
+
 // step 2: get bookings
     // bookings have a key where the userID will match the customer.id
     // iterate through all bookings.userID to filter the ids that match
     // return an array of booking objects matching criteria
+
+
+
 // step 3: display filtered bookings in table
     // in dom updates
+
+
+
 // step 4: display total amount spent on rooms
     // use our filtered bookings array
     // iterate through rooms to match rooms.number to bookings.roomNumber
