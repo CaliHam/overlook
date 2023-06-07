04041864fbc8a70b23c69d8c3de369693388a8ee
Calli Herrmann <calliherrmann@gmail.com>
2023-06-07 13:01:15 -0400

    Add basic table for dashboard view and new dom updates file

--- src/scripts.js
+++ src/scripts.js
@@ -5,7 +5,7 @@
 import './scss/styles.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-// import './images/Overlook-logo.png'
+import '../dist/images/Overlook-logo.png'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
