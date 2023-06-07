bbea84f16df45cb0c84e8b6292b6845627f3ebc3
Calli Herrmann <calliherrmann@gmail.com>
2023-06-07 14:43:01 -0400

    Update sample data bookings to match sample customers

--- src/data/sample-bookings.js
+++ src/data/sample-bookings.js
@@ -2,19 +2,19 @@ const bookingRequest = {
     "bookings": [
         {
         "id": "5fwrgu4i7k55hl6sz",
-        "userID": 9,
+        "userID": 1,
         "date": "2022/04/22",
         "roomNumber": 15
         },
         {
         "id": "5fwrgu4i7k55hl6t5",
-        "userID": 43,
+        "userID": 2,
         "date": "2022/01/24",
         "roomNumber": 24
         },
         {
         "id": "5fwrgu4i7k55hl6t6",
-        "userID": 13,
+        "userID": 3,
         "date": "2022/01/10",
         "roomNumber": 12
         }
