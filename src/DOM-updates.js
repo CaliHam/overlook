eca704fb3e56816a6c29bd030ee2deb8f43d2c85
Calli Herrmann <calliherrmann@gmail.com>
2023-06-07 17:57:01 -0400

    Sort bookings by newest to oldest date

--- src/DOM-updates.js
+++ src/DOM-updates.js
@@ -2,12 +2,17 @@ const bookingTable = document.querySelector('#booking-info');
 const totalCost = document.querySelector('#total-cost')
 
 const displayBookings = (bookings, rooms) => {
+    bookings.sort((a, b) => {
+        const dateA = new Date(a.date);
+        const dateB = new Date(b.date);
+        return dateB - dateA;
+      });
     bookings.forEach((booking, i)=> {
         bookingTable.innerHTML += `
             <tr id="${booking.id}">
                 <td>${booking.roomNumber}</td>
                 <td>${booking.date}</td>
-                <td>${rooms[i].costPerNight}</td>
+                <td>$${rooms[i].costPerNight}</td>
             </tr>`
     })
 }
