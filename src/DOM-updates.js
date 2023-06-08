2353cd6b6bcf3c3b2e85c94b3a331a6253a48c58
Calli Herrmann <calliherrmann@gmail.com>
2023-06-08 18:54:24 -0400

    Add styling to availble rooms

--- src/DOM-updates.js
+++ src/DOM-updates.js
@@ -12,6 +12,7 @@ const dashBtn = document.querySelector('#dash-btn')
 const bookingBtn = document.querySelector('#book-btn')
 const calendar = document.querySelector('#calendar')
 const checkDateBtn = document.querySelector('#check-date')
+const searchResults = document.querySelector('#results')
 
 // Date Picker //
 const picker = datepicker(calendar)
@@ -68,11 +69,28 @@ const displayTotal = (cost) => {
 	totalCost.innerHTML = `<b>${cost}</b>`
 }
 
-
+const displayAvailableRooms = (availableRooms, date) => {
+	searchResults.classList.remove('hidden')
+	searchResults.innerHTML = ''
+	if(!availableRooms.length){
+		searchResults.innerHTML = `<p>Sorry there are no rooms available on ${date} at this time.</p>`
+	}
+	availableRooms.forEach(room => {
+		searchResults.innerHTML += `
+		<div class="room-result-container">
+			<h3>${room.roomType} with ${room.numBeds} ${room.bedSize} bed</h3>
+			<p class="bidet">Bidet ${room.bidet ? 'is' : 'not'} included.</p>
+			<hr>
+			<p class="cost-per-night"><b>$${room.costPerNight.toFixed(2)}</b></p>
+			<button class="book-room-btn">Book</button
+		</div>`
+	})
+}
 
 
 export {
 	displayBookings,
 	displayTotal,
-	displayUsername
+	displayUsername,
+	displayAvailableRooms
 }
--- src/scripts.js
+++ src/scripts.js
@@ -7,7 +7,7 @@ import dayjs from 'dayjs';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import '../dist/images/Overlook-logo.png'
 import { getData, getAllData } from './apiCalls';
-import { displayBookings, displayTotal, displayUsername } from './DOM-updates'
+import { displayBookings, displayTotal, displayUsername, displayAvailableRooms } from './DOM-updates'
 
 let currentCustomer;
 let currentBookings;
@@ -71,26 +71,29 @@ const getTotalCost = (rooms) => {
 }
 
 // ✅ I should be able to select a date for which I’d like to book a room for myself
-// Upon selecting a date, I should be shown a list of room details for only rooms that are available on that date
+// ✅ Upon selecting a date, I should be shown a list of room details for only rooms that are available on that date
 // I should be able to filter the list of available rooms by their roomType property
 // I should be able to select a room for booking
 // In the event that no rooms are available for the date/roomType selected, display a message fiercely 
-// apologizing to the user and asking them to adjust their room search
+	// apologizing to the user and asking them to adjust their room search
 
 const checkAvailability = (date) => {
 	const formattedDate = dayjs(date).format('YYYY/MM/DD')
 	const allRoomNumbers = allRooms.map(room => room.number)
 	const unavailableRooms = allBookings.filter(booking => booking.date === formattedDate).map(room => room.roomNumber)
 	const roomsReady = allRoomNumbers.filter(room => !unavailableRooms.includes(room))
-	console.log(roomsReady)
 	const availableRooms = roomsReady.reduce((acc, currNum) => {
 		let foundRoom = allRooms.find(room => room.number === currNum)
 		acc.push(foundRoom)
 		return acc
 	},[])
-	console.log(availableRooms)
+	displayAvailableRooms(availableRooms, date)
+	// filterRooms(availableRooms)
 }
 
+// const filterRooms = (rooms) => {
+
+// }
 
 export {
 	checkAvailability
