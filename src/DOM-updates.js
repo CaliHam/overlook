636952336fd0ab9a715c5fc8df75755724eb5efa
Calli Herrmann <calliherrmann@gmail.com>
2023-06-08 11:40:06 -0400

    Add button functionality to switch between dashboard and booking page

--- src/DOM-updates.js
+++ src/DOM-updates.js
@@ -2,33 +2,56 @@ const wholeTable = document.querySelector('#customer-bookings')
 const bookingTable = document.querySelector('#booking-info');
 const totalCost = document.querySelector('#total-cost')
 const userMenu = document.querySelector('.user')
+const dashView = document.querySelector('#dashboard-view')
+const bookingView = document.querySelector('#booking-view')
+const dashBtn = document.querySelector('#dash-btn')
+const bookingBtn = document.querySelector('#book-btn')
+
+// EVENT LISTENERS
+
+dashBtn.addEventListener('click', () => {
+	hideAllPages()
+	dashView.classList.remove('hidden')
+})
+
+bookingBtn.addEventListener('click', () => {
+	hideAllPages()
+	bookingBtn.classList.remove('hidden')
+})
+
+// CODE
 
 const displayUsername = (user) => {
-    userMenu.innerText = user.name
+	userMenu.innerText = user.name
+}
+
+const hideAllPages = () => {
+	dashView.classList.add('hidden')
+	bookingView.classList.add('hidden')
 }
 
 const displayBookings = (bookings, rooms) => {
-    if(!bookings){
-        wholeTable.innerHTML = `<th>Book some rooms the view them here!</th>`
-        return
-    }
-    bookings.sort((a, b) => {
-        const dateA = new Date(a.date);
-        const dateB = new Date(b.date);
-        return dateB - dateA;
-      });
-    bookings.forEach((booking, i)=> {
-        bookingTable.innerHTML += `
-            <tr id="${booking.id}">
-                <td>${booking.roomNumber}</td>
-                <td>${booking.date}</td>
-                <td>$${rooms[i].costPerNight}</td>
-            </tr>`
-    })
+	if(!bookings){
+		wholeTable.innerHTML = `<th>Book some rooms the view them here!</th>`
+		return
+	}
+	bookings.sort((a, b) => {
+		const dateA = new Date(a.date);
+		const dateB = new Date(b.date);
+		return dateB - dateA;
+	});
+	bookings.forEach((booking, i)=> {
+		bookingTable.innerHTML += `
+			<tr id="${booking.id}">
+				<td>${booking.roomNumber}</td>
+				<td>${booking.date}</td>
+				<td>$${rooms[i].costPerNight}</td>
+			</tr>`
+	})
 }
 
 const displayTotal = (cost) => {
-    totalCost.innerHTML = `<b>${cost}</b>`
+	totalCost.innerHTML = `<b>${cost}</b>`
 }
 
 
--- src/scripts.js
+++ src/scripts.js
@@ -12,48 +12,48 @@ let currentBookings;
 let bookedRooms;
 
 window.onload = () => {
-    getData('customers/20')
-    .then(response => {
-        currentCustomer = response
-        getCurrentBookings(currentCustomer)
-        displayUsername(currentCustomer)
-    })
+	getData('customers/20')
+		.then(response => {
+			currentCustomer = response
+			getCurrentBookings(currentCustomer)
+			displayUsername(currentCustomer)
+	})
 }
 
 // DASHBOARD VIEW
 
 const getCurrentBookings = (currentCustomer) => {
-    getData('bookings')
-    .then(response => {
-        currentBookings = response.bookings.filter(booking => booking.userID === currentCustomer.id)
-        getBookedRooms(currentBookings)
-    })
+	getData('bookings')
+		.then(response => {
+			currentBookings = response.bookings.filter(booking => booking.userID === currentCustomer.id)
+			getBookedRooms(currentBookings)
+		})
 }
 
 const getBookedRooms = (currentBookings) => {
-    getData('rooms')
+	getData('rooms')
     .then(response => {
-        const roomNumbers = currentBookings.map(booking => booking.roomNumber)
-        bookedRooms = roomNumbers.reduce((foundRooms, currRoom) => {
-            const foundRoom = response.rooms.find(room => room.number === currRoom)
-            foundRooms.push(foundRoom)
-            return foundRooms
+			const roomNumbers = currentBookings.map(booking => booking.roomNumber)
+			bookedRooms = roomNumbers.reduce((foundRooms, currRoom) => {
+					const foundRoom = response.rooms.find(room => room.number === currRoom)
+					foundRooms.push(foundRoom)
+					return foundRooms
         }, [])
-        displayTotal(getTotalCost(bookedRooms))
-        displayBookings(currentBookings, bookedRooms)
+			displayTotal(getTotalCost(bookedRooms))
+			displayBookings(currentBookings, bookedRooms)
     })
 }
 
 const getTotalCost = (rooms) => {
-    const totalCost = rooms.reduce((cost, currRoom) => {
-        cost += currRoom.costPerNight;
-        return cost
-    }, 0)
-    const formattedCost = totalCost.toLocaleString('en-US', {
-        style: 'currency',
-        currency: 'USD'
-    });
-    return formattedCost
+	const totalCost = rooms.reduce((cost, currRoom) => {
+		cost += currRoom.costPerNight;
+		return cost
+	}, 0)
+	const formattedCost = totalCost.toLocaleString('en-US', {
+		style: 'currency',
+		currency: 'USD'
+	});
+	return formattedCost
 }
 
 
