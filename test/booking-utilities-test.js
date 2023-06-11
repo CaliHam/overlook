1ac8efa4441846fe662d405e45f989eb3ac6589e
Calli Herrmann <calliherrmann@gmail.com>
2023-06-11 19:40:02 -0400

    Add one test per function to pass

--- test/booking-utilities-test.js
+++ test/booking-utilities-test.js
@@ -3,7 +3,7 @@ import chai from 'chai';
 import { sampleCustomers } from '../src/data/sample-customers';
 import { sampleRooms } from '../src/data/sample-rooms';
 import { sampleBookings } from '../src/data/sample-bookings';
-import { getTotalCost } from '../src/booking-utilities';
+import { getTotalCost, validateDate, findRooms } from '../src/booking-utilities';
 
 
 const expect = chai.expect;
@@ -19,8 +19,53 @@ describe('Retrieve cost', function() {
     
   // })
   it('should return a cost given an array of room objects', function() {
-    // const totalCost = getTotalCost(sampleRooms)
-    // console.log(totalCost)
-    // expect(totalCost).to.be.a.string;
+    const totalCost = getTotalCost(sampleRooms.rooms)
+
+    expect(totalCost).to.equal('$1,326.92')
+  });
+});
+
+describe('Get date', function() {
+  // let customer1;
+
+  // beforeEach(() => {
+    
+  // })
+  it('should return a date in YYYY/MM/DD format', function() {
+    const today = 'June 11 2023'
+    const formattedDate = validateDate(today)
+
+    expect(formattedDate).to.equal('2023/06/11')
+  });
+});
+
+describe('Find rooms', function() {
+  // let customer1;
+
+  // beforeEach(() => {
+    
+  // })
+  it('should return rooms that match numbers given', function() {
+    const roomNumbers = [15, 24]
+    const allRooms = sampleRooms.rooms
+    const foundRooms = findRooms(roomNumbers, allRooms)
+
+    expect(foundRooms).to.deep.equal([
+      {
+      "number": 15,
+      "roomType": "residential suite",
+      "bidet": true,
+      "bedSize": "queen",
+      "numBeds": 1,
+      "costPerNight": 358.4
+      },
+      {
+      "number": 24,
+      "roomType": "suite",
+      "bidet": false,
+      "bedSize": "full",
+      "numBeds": 2,
+      "costPerNight": 477.38
+      },])
   });
 });
